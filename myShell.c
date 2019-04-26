5e7b4acc199a1f85861208d5cf4cb86397829750
kgh940525 <kgh940525@gmail.com>
2019-04-26 18:43:50 +0900

    [my_cwd] my path-pwd function

---
+++ myShell.c
@@ -0,0 +1,27 @@
+
+int main(int argc,char* argv[])
+{
+    
+    
+    // 현재 경로 구하기
+    char buf[BUFSIZ];
+    char host_name[256] = {0x00};
+    char user_name[256] = {0x00};
+    char line[256] = {0x00};
+    int len = 256;
+    char *cwd;
+    
+    cwd = getcwd(NULL,BUFSIZ);
+    getlogin_r(user_name,len);
+    gethostname(host_name,len);
+    printf("%s \n", user_name);
+    printf("%s \n",host_name);
+    printf("%s \n",cwd);
+
+
+    
+    
+    
+    return 0;
+}
+
