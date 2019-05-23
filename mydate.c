3881dc385e5eded67487b4aec83239c1b23845ee
Gwan-Hyeon <kgh940525@gmail.com>
2019-05-24 01:41:00 +0900

    [mymv comment updated]

--- mydate.c
+++ mydate.c
@@ -1,14 +1,14 @@
-#include <sys/time.h>
-#include <unistd.h>
-#include <stdio.h>
-#include <time.h>
+#include <sys/time.h>		// 시스템 시간 라이브러리
+#include <unistd.h>		// 접근권한 라이브러리
+#include <stdio.h>		// 입출력 라이브러리
+#include <time.h>		// 시간 라이브러리
 int main(void)
 {
-    time_t curr;
-    struct tm *d;
-    curr=time(NULL);
-    d=localtime(&curr);
-    printf("%s", asctime(d));
+    time_t curr;	 // 현재 시간 가져오기위함
+    struct tm *store;	// 타임 함수 구조체 사용
+    curr=time(NULL);	// 시간 값 초기화
+    store=localtime(&curr);	// 로컬 타임 저장
+    printf("%s", asctime(store)); // 로컬 타임 출력
     return 0;
 }
 
--- mymv.c
+++ mymv.c
@@ -1,40 +1,40 @@
-#include <stdio.h>      // stdin/out library
-#include <stdlib.h>     // string library
-#include <unistd.h>     // acccess library
-#include <errno.h>      // error check library
-#include <string.h>     // string library
+#include <stdio.h>      // 입출력 라이브러리
+#include <stdlib.h>     // 문자열 라이브러리
+#include <unistd.h>     // 접근권한 라이브러리
+#include <errno.h>      // 에러처리 라이브러리
+#include <string.h>     // 문자열 라이브러리
 int main(int argc,char *argv[])
 {
 	char* pathname;
-	// arguments check in command line	
+	// 명령어 인자 체크(성공)	
 	if(argc==3){
-		// //check if file to be moved exists
+		//파일 접근 체크
 		if((access(argv[1],F_OK|W_OK))==0){
-			// append name of the file to the given path name
+			// 인자 경로값 저장
 			pathname = strcat(argv[2], argv[1]);
-			// if link is succesfully created
+			// 파일 이동 성공 확인
 			if(link(argv[1],argv[2])==0){
-				// if file argv[1] is deleted from its original location
+				// 파일 unlink 처리
 				if((unlink(argv[1]))==0){
 					printf("%s succesfully moved to %s\n",argv[1],argv[2]);
 				}
-                // Link Deletion error
+				//파일 unlink 에러처리
 				else{
 					
 					perror("Link deletion error");
 				}
 			}
-			// Link creation error
+			// 링크 생성 에러
 			else{
 				perror("Link creation error");
 			}
 		}
-		// File Error
+		// 파일 에러
 		else{
 			perror("File error");
 		}
 	}
-	// invalid number ofargument
+	// 인자값 체크 실패
 	else{
 		printf("Invalid number of arguments : <command> <filename> <pathname/>\n");
 	}
