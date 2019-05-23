313589b153b9fb8afcddf7087d9cbd76fb0086bd
kgh940525 <kgh940525@gmail.com>
2019-05-23 20:44:18 +0900

    [korean language comment update to english]

--- mymv.c
+++ mymv.c
@@ -1,44 +1,43 @@
-#include <stdio.h>      // ¿¿¿¿¿ ¿¿¿¿¿
-#include <stdlib.h>     // ¿¿¿¿¿¿¿¿
-#include <unistd.h>     // ¿¿¿¿ ¿¿¿¿¿
-#include <errno.h>      // ¿¿¿¿ ¿¿¿¿¿
-#include <string.h>     // string ¿¿¿ ¿¿¿¿¿
+#include <stdio.h>      // stdin/out library
+#include <stdlib.h>     // string library
+#include <unistd.h>     // acccess library
+#include <errno.h>      // error check library
+#include <string.h>     // string library
 int main(int argc,char *argv[])
 {
 	char* pathname;
-	// ¿¿¿ ¿¿ ¿¿
+	// arguments check in command line	
 	if(argc==3){
-		// ¿¿ ¿¿¿ ¿ ¿¿ ¿¿ ¿¿¿¿ ¿¿ 
+		// //check if file to be moved exists
 		if((access(argv[1],F_OK|W_OK))==0){
-			// ¿¿ path ¿ ¿¿
+			// append name of the file to the given path name
 			pathname = strcat(argv[2], argv[1]);
-			// ¿¿ ¿¿¿ ¿¿¿¿ ¿¿ 
+			// if link is succesfully created
 			if(link(argv[1],argv[2])==0){
-				// ¿¿ ¿¿ ¿¿¿¿ ¿¿ 
+				// if file argv[1] is deleted from its original location
 				if((unlink(argv[1]))==0){
 					printf("%s succesfully moved to %s\n",argv[1],argv[2]);
 				}
-                // ¿¿ ¿¿ ¿¿
+                // Link Deletion error
 				else{
 					
 					perror("Link deletion error");
 				}
 			}
-			// ¿¿ ¿¿ ¿¿
+			// Link creation error
 			else{
 				perror("Link creation error");
 			}
 		}
-		// ¿¿¿¿
+		// File Error
 		else{
 			perror("File error");
 		}
 	}
-	// ¿¿¿ ¿¿ ¿¿ 
+	// invalid number ofargument
 	else{
 		printf("Invalid number of arguments : <command> <filename> <pathname/>\n");
 	}
 
 
 }
-
